50dc5c03fa321170682ebe0fd59f19a3243556df
수현 <sxhlxxn@gachon.ac.kr>
2026-05-19 03:30:45 +0900

    fix: update socket server url to load balancer port

--- Client/src/socket/socketClient.js
+++ Client/src/socket/socketClient.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-export const socketClient = io('http://localhost:3000', {
+export const socketClient = io('http://localhost:80', {
   transports: ['websocket', 'polling'],
   reconnection: true,
   reconnectionAttempts: 5,
