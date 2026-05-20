c550a5a4ca45b312a351bb05cee14c410b569cf6
Park Pilju <sohiduddo@naver.com>
2026-05-20 22:51:09 +0900

    feat: add isAnswer flag to checkAnswer

--- Server/gameManager.js
+++ Server/gameManager.js
@@ -117,8 +117,8 @@ const assignTopic = async (client, roomId) => {
  * @param {string} message
  * @returns {object} { isCorrect, player, point, scores, allCorrect }
  */
-const checkAnswer = async (client, roomId, playerId, message, isAnswer = false) => {
-    if (!isAnswer) return null;
+const checkAnswer = async (client, roomId, playerId, message, inAnswer = false) => {
+    if (!inAnswer) return null;
     const gameState = await getGameState(client, roomId);
     if (!gameState) return null;
 
